a7dee120fcd37fa316526c8d9b17d49d02d5315a
flacroix-ubern <119116449+flacroix-ubern@users.noreply.github.com>
2022-12-27 12:58:34 +0100

    Add files via upload

---
+++ soil_constants.py
@@ -0,0 +1,8 @@
+#Misc
+Tzero = 273.15 # [K] Freezing temperature
+
+#Water/ice/vapor properties
+rel_vol_w2ice = 997 / 917 # ratio water to ice
+w_dens = 997 # [kg / m3] water density
+latent_heat_fusion = 333.15 # [kg / kg] Latent heat of water fusion
+
